62526f5f7ae5a0ddae0ba85038fb689140f1c398
hassankbrian <hassankbrian@gmail.com>
2015-11-22 13:35:42 -0500

    Adding preprocessing bus stops

--- src/PreProcess.py
+++ src/PreProcess.py
@@ -1,7 +1,7 @@
 import numpy
 
 Buses = [1]
-BusStops = [[1, 1], [2, 2]]
+BusStops = [("Cherry", 33.772342, -84.395699), ("North Ave", 33.770062, -84.391629), ("Smith Dorm", 33.771664, -84.391992), ("Techwood and Bobby Dodd", 33.774098, -84.391979), ("Techwood and Fourth", 33.775346, -84.391958),("Techwood and Fifth", 33.776797, -84.392062)]
 Data = recfromcsv('../data/data.csv')
 print('About to print data:\n')
 Data
